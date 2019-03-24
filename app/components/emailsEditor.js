59b0cac46fa6e703552281d7e2b144595ef3bf5b
Shawn59 <xxx_respekt.ru@mail.ru>
2019-03-24 22:20:56 +0500

    #master - fix

--- app/components/emailsEditor.js
+++ app/components/emailsEditor.js
@@ -2,12 +2,15 @@ appRoute.directive('emailsEditor', function ($mdConstant) {
     // одна из регулярок на проверку формата email из кучи разных вариаций))
     var EMAIL_PATTERN = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
     return {
-        //изолир скоуп
+        //изоляция скоупа
         scope: {},
+        // директива работает только в качестве тега
+        restrict: 'E',
         templateUrl: 'app/components/emailsEditor.html',
         link: function (scope, element, attrs) {
             scope.listEmails = [];
             scope.name = attrs.name || 'emailsEditorChips';
+            //require
             scope.id = attrs.id || 'emailsEditorChipsId';
 
             //массив ключей для формирования чипа
@@ -15,6 +18,7 @@ appRoute.directive('emailsEditor', function ($mdConstant) {
                 $mdConstant.KEY_CODE.ENTER,
                 $mdConstant.KEY_CODE.COMMA
             ];
+
             scope.changeChips = function (chip, index) {
                 var chipName = chip;
                 angular.element(document.querySelector('#' + scope.id)).ready(function () {
@@ -25,6 +29,13 @@ appRoute.directive('emailsEditor', function ($mdConstant) {
                 });
             };
 
+            /*s = element.find('input');
+
+            element.find('input').keydown(function(e) {
+                if (ctrlDown && (e.keyCode == cKey)) console.log("Document catch Ctrl+C");
+                if (ctrlDown && (e.keyCode == vKey)) console.log("Document catch Ctrl+V");
+            });
+*/
             scope.addRandomEmail = function () {
                 var email = Math.random().toString(36).substr(2, 7) + '@gmail.com';
                 // оригинальная директива chips крошится при добавлении дубликата
